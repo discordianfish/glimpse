0fc66d5f4c35a84310d564d580ada70d8369dd51
Alexander Simmerl <alx@soundcloud.com>
2013-03-28 20:58:33 +0100

    Add license and high-level doc

--- endpoint.go
+++ endpoint.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2013, SoundCloud Ltd.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Source code and contact info at http://github.com/soundcloud/glimpse
+
 package glimpse
 
 import (
