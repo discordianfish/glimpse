6b7f656f40a33f310797cb91370454b5db9a3de3
Tobias Schmidt <ts@soundcloud.com>
2015-02-26 02:19:47 -0500

    Rate logging and UDP by default

--- stare/main.go
+++ stare/main.go
@@ -2,72 +2,90 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"runtime"
+	"sync/atomic"
+	"time"
 
 	"github.com/miekg/dns"
 )
 
 func main() {
 	var (
-		a = flag.String("addr", "ns0.sd.int.s-cloud.net:53", "glimpse agent addr")
-		c = flag.Int("c", 50, "DNS query concurrency")
+		addr        = flag.String("addr", "localhost:53", "DNS server address")
+		concurrency = flag.Int("c", 50, "DNS query concurrency")
+		query       = flag.String("q", "", "test query")
+		logging     = flag.Bool("log", false, "log errors")
+		net         = flag.String("net", "udp", "DNS network protocol")
 	)
 	flag.Parse()
 
-	tests := []string{
-		"telemetry.kraken-api.prod.timeline.dd.sd.int.s-cloud.net.",
-		"telemetry.agent.prod.glimpse.dd.sd.int.s-cloud.net.",
-		"http.api.prod.v2.dd.sd.int.s-cloud.net.",
+	if *query == "" {
+		fmt.Fprint(os.Stderr, "flag must be provided: -q\n")
+		flag.Usage()
+		os.Exit(1)
 	}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	d := newClient(*a)
-	p := make(chan struct{}, *c)
-	i := 0
-	for {
-		for _, q := range tests {
-			p <- struct{}{}
-			go d.query(q, p)
-			i++
+	var (
+		client = newClient(*addr, *net)
+		pool   = make(chan struct{}, *concurrency)
 
-			if i%*c == 0 {
-				log.Printf("%d requests", *c)
-			}
+		requests, errors uint64
+	)
+
+	go func() {
+		for {
+			pool <- struct{}{}
+			go func(q string) {
+				if err := client.query(q); err != nil {
+					if *logging {
+						log.Print(err)
+					}
+					atomic.AddUint64(&errors, 1)
+					<-time.After(100 * time.Millisecond)
+				}
+				<-pool
+			}(*query)
+			atomic.AddUint64(&requests, 1)
 		}
+	}()
+
+	var lastr, laste uint64
+	for range time.Tick(time.Second) {
+		r, e := atomic.LoadUint64(&requests), atomic.LoadUint64(&errors)
+		fmt.Printf("\r%s [%d r/s, %d err/s]", *query, r-lastr, e-laste)
+		lastr, laste = r, e
 	}
 }
 
 type client struct {
-	addr string
+	addr, net string
 
 	*dns.Client
 }
 
-func newClient(addr string) client {
+func newClient(addr, net string) client {
 	return client{
 		addr:   addr,
-		Client: &dns.Client{Net: "tcp"},
+		Client: &dns.Client{Net: net},
 	}
 }
 
-func (c client) query(q string, p chan struct{}) {
-	defer func() { <-p }()
-
+func (c client) query(q string) error {
 	m := &dns.Msg{}
 	m.SetQuestion(q, dns.TypeSRV)
 
 	r, _, err := c.Client.Exchange(m, c.addr)
 	if err != nil {
-		log.Printf("failed %s: %s", q, err)
-		return
+		return fmt.Errorf("failed %s: %s", q, err)
 	}
 	if dns.RcodeSuccess != r.Rcode {
-		log.Printf(
-			"failed %s: unexpected %s",
+		return fmt.Errorf("failed %s: unexpected %s",
 			q, dns.RcodeToString[r.Rcode])
-		return
 	}
-	//log.Printf("success %s: %d answers", q, len(r.Answer))
+	return nil
 }
